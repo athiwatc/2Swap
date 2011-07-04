2254ee53e86251348e59d3e3ae23a93941e6dc81
Athiwat Chunlakhan <athiwatc@gmail.com>
2011-07-04 11:42:03 +0700

    Start

---
+++ 2Swap/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  2Swap
+//
+//  Created by Athiwat Chunlakhan on 7/4/11.
+//  Copyright Kasetsart University 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
