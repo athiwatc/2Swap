782fdf16ed49724a962b0255bb31905921eaa61c
Saran Siripuekpong <anti7gn@gmail.com>
2011-07-14 14:23:06 +0700

    storymodescene
    change the background in storymodescene

--- 2Swap/StoryModeScene.m
+++ 2Swap/StoryModeScene.m
@@ -24,14 +24,17 @@
     if ((self = [super init]))
     {
         CCLOG(@"%@: %@", NSStringFromSelector(_cmd), self);
+        CCSprite* bg = [CCSprite spriteWithFile:@"Story_mode_screen.png"];
 		CCSprite* level1 = [CCSprite spriteWithFile:@"Story_mode_but.png"];
 		CCLabelTTF* label = [CCLabelTTF labelWithString:@"Story Mode Scene" fontName:@"Marker Felt" fontSize:64];
 		label.color = ccRED;
 		CGSize size = [[CCDirector sharedDirector] winSize];
+        bg.position = CGPointMake(size.width / 2, size.height / 2);
 		label.position = CGPointMake(size.width / 2, size.height / 2);
         level1.position = CGPointMake(size.width / 2, size.height / 2 + 100);
-		[self addChild:label];
-        [self addChild:level1];
+		[self addChild:label z:1];
+        [self addChild:level1 z:1];
+        [self addChild:bg z:0];
 		
 		self.isTouchEnabled = YES;
     }
